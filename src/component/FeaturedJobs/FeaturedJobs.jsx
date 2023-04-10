e4c92c48ae8e40df9176c3c0e579e4a8aaed7d20
KKD98 <KajolKona98@gmail.com>
2023-04-10 14:15:53 +0600

    Shows all 6 jobs when Show all jobs button is clicked

--- src/component/FeaturedJobs/FeaturedJobs.jsx
+++ src/component/FeaturedJobs/FeaturedJobs.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 import SingleFeaturedJob from '../SingleFeaturedJob/SingleFeaturedJob';
 
 const FeaturedJobs = () => {
+    const [featuredjobs , setFeaturedJobs] = useState(false);
     const allFeaturedJobs = useLoaderData();
     const fourFeatures = allFeaturedJobs.slice(0 , 4);
     return (
@@ -10,11 +11,16 @@ const FeaturedJobs = () => {
          <h1 className='text-center text-gray-950 text-4xl font-bold'>Featured Jobs</h1>
         <p className='text-center text-gray-500 my-5'>Explore thousands of job opportunities with all the information you need. Its your future.</p>
            <div className='grid grid-cols-1 md:grid-cols-2 gap-5 w-full md:w-[80%] mx-auto my-3 p-5'>
-           {fourFeatures.map(featuredJob => <SingleFeaturedJob key={featuredJob.id} featuredJob={featuredJob}></SingleFeaturedJob>)}
+           {
+            featuredjobs === true ? allFeaturedJobs.map(featuredJob => <SingleFeaturedJob key={featuredJob.id} featuredJob={featuredJob}></SingleFeaturedJob>) : fourFeatures.map(featuredJob => <SingleFeaturedJob key={featuredJob.id} featuredJob={featuredJob}></SingleFeaturedJob>)
+           }
+           {/* {fourFeatures.map(featuredJob => <SingleFeaturedJob key={featuredJob.id} featuredJob={featuredJob}></SingleFeaturedJob>)} */}
            </div>
-           <Link className='flex'>
-            <button className='mx-auto bg-gradient-to-r from-violet-400 to-indigo-400 text-white p-3 rounded w-[35%] md:w-[10%]'>See All Jobs</button>
-           </Link>
+           <div className='flex'>
+            <button className='mx-auto bg-gradient-to-r from-violet-400 to-indigo-400 text-white p-3 rounded w-[35%] md:w-[10%]'
+            onClick={()=> setFeaturedJobs(!featuredjobs)}
+            >{featuredjobs === true ? 'Back' : 'See All Jobs'}</button>
+           </div>
         </div>
     );
 };
